7abbbf5d8bb2ba648980fb8f9427211738515635
PumpkinXD <54535387+PumpkinXD@users.noreply.github.com>
2024-10-14 00:51:39 +0800

    "cherry-picking" 8612b29c3a4324da5fc58642b3a041cad5937011 - add a virtual cursor for wayland - known issue: 1.Typing Key E in Search Tab of the Creative Inventory will close it; 2. cursor still unable move out the game window(under wayland)

--- src/main/java/com/github/zarzelcow/legacylwjgl3/DesktopFileInjector.java
+++ src/main/java/com/github/zarzelcow/legacylwjgl3/DesktopFileInjector.java
@@ -4,7 +4,6 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -15,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.github.zarzelcow.legacylwjgl3.util.XDGPathResolver;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.math.MathHelper;
 import org.apache.commons.io.IOUtils;
@@ -81,24 +81,12 @@ public class DesktopFileInjector {
 
 
 	private static Path getIconFileLocation(int width, int height) {
-		return getDataLocation().resolve("icons/hicolor").resolve(width + "x" + height)
+		return XDGPathResolver.getUserDataLocation().resolve("icons/hicolor").resolve(width + "x" + height)
 				.resolve("apps").resolve(ICON_NAME);
 	}
 
 	private static Path getDesktopFileLocation() {
-		return getDataLocation().resolve("applications").resolve(FILE_NAME);
-	}
-
-	private static Path getDataLocation() {
-		String xdgDataHome = System.getenv("$XDG_DATA_HOME");
-		if (xdgDataHome == null || xdgDataHome.isEmpty()) {
-			String home = System.getenv().getOrDefault("$HOME", System.getProperty("user.home"));
-			if (home == null || home.isEmpty()) {
-				throw new IllegalStateException("could not resolve user home");
-			}
-			return new File(home).toPath().resolve(".local/share/");
-		}
-		return new File(xdgDataHome).toPath();
+		return XDGPathResolver.getUserDataLocation().resolve("applications").resolve(FILE_NAME);
 	}
 
 	private static void updateIconSystem() {
---
+++ src/main/java/com/github/zarzelcow/legacylwjgl3/mixin/MixinMinecraftDrawVirtualCursor.java
@@ -0,0 +1,17 @@
+package com.github.zarzelcow.legacylwjgl3.mixin;
+
+import com.github.zarzelcow.legacylwjgl3.implementation.glfw.VirtualGLFWMouseImplementation;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MixinMinecraftDrawVirtualCursor {
+
+	@Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/AchievementNotification;tick()V"))
+	private void drawVirtualCursor(CallbackInfo ci) {
+		VirtualGLFWMouseImplementation.render();
+	}
+}
