ad92cd0fed8792cac6a4880905150b95253fa908
moehreag <moehreag@gmail.com>
2025-01-20 22:51:27 +0100

    Initialize glfw earlier

--- src/main/java/com/github/zarzelcow/legacylwjgl3/mixin/MixinMinecraftFixFullscreenResize.java
+++ src/main/java/com/github/zarzelcow/legacylwjgl3/mixin/MixinMinecraftFixFullscreenResize.java
@@ -1,15 +1,19 @@
 package com.github.zarzelcow.legacylwjgl3.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftClient.class)
 public class MixinMinecraftFixFullscreenResize {
 
+    @Shadow private boolean fullscreen;
+
     @Redirect(method = "updateWindow", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;fullscreen:Z"))
     private boolean noFullscreenCheckForResize(MinecraftClient instance) {
-        return false;
+        return GLFW.glfwGetPlatform() == GLFW.GLFW_PLATFORM_WIN32 && fullscreen;
     }
 }
